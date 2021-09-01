356bba0d640b62750ec1a8135b9eba1db240253b
FrowsyFrog <frowsyfrog@gmail.com>
2021-09-01 16:42:12 -0500

    Added Alert

--- FactorizacionLU/FactorizacionLU/FactorizacionLU.cpp
+++ FactorizacionLU/FactorizacionLU/FactorizacionLU.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <locale.h>
+
+void alertNotification(int min, int max) {
+
+    printf("\033[1;31m"); // Cambiar Texto Color Rojo
+    printf("¡El número debe pertenecer al rango: [%d-%d]!\n", min, max);
+    printf("\033[0m"); // Reset Color
+}
+
+bool inRange(unsigned x, int min, int max) {
+    return (x - min) <= (max - min);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, ""); // Función para que se impriman las tildes
+
+    int x = 15, min = 4, max = 10;
+    if (!inRange(x, min, max)) {
+        alertNotification(min, max);
+    }
 }
